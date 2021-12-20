ad876b2eff866844ae5957ea3a08729539af76e3
mohamedmostafabadran <mohamedmostafabadran@yahoo.com>
2021-12-21 00:33:20 +0200

    added gui

---
+++ Implementation/src/main/java/com/swe2023/gui/AdminFlightController.java
@@ -0,0 +1,84 @@
+package com.swe2023.gui;
+
+import com.swe2023.HelloApplication;
+import com.swe2023.model.Planes_Data.Flight;
+import com.swe2023.model.Planes_Data.Plane;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class AdminFlightController {
+    @FXML
+    public Button updateButton;
+    public TextField IDField;
+    public ChoiceBox PlaneIDField;
+    public ChoiceBox SourceField;
+    public ChoiceBox DestinationField;
+    public DatePicker DateField;
+
+
+    public ListView<String> listView;
+
+    private ArrayList<Flight> Flights;
+    private Flight flight;
+    private ArrayList<String> Ports;
+    private ArrayList<String> Planes;
+
+
+    public void initialize() {
+        Flights = new ArrayList<>();
+        Ports = new ArrayList<>();
+        Planes = new ArrayList<>();
+        Ports.add("paris");
+        Ports.add("cairo");
+        Ports.add("london");
+        Ports.add("new york");
+        Planes.add("1");
+        Planes.add("2");
+        Planes.add("3");
+
+        for (int i = 0; i < Ports.size(); i++) {
+            SourceField.getItems().add(Ports.get(i));
+            DestinationField.getItems().add(Ports.get(i));
+        }
+        for (int i = 0; i < Planes.size(); i++)
+            PlaneIDField.getItems().add(Planes.get(i));
+    }
+    public void goToAdminHome() {
+        HelloApplication.showWindow(updateButton, "/admin-home.fxml", "Administrator", 800,640);
+    }
+
+    public void createFlight(ActionEvent actionEvent) {
+        Flight flight= new Flight(IDField.getText(),(String) PlaneIDField.getSelectionModel().getSelectedItem(),
+                (String)SourceField.getSelectionModel().getSelectedItem()
+        ,(String)DestinationField.getSelectionModel().getSelectedItem(),DateField.getValue());
+        Flights.add(flight);
+        updateListView();
+    }
+
+    public void updateListView(){
+        listView.getItems().clear();
+        for(Flight i : Flights)
+            listView.getItems().add(i.getFlightID());
+        listView.getItems().removeAll();
+    }
+
+
+    public void getViewItem() {
+        int index= listView.getSelectionModel().getSelectedIndex();
+        Flight flight= Flights.get(index);
+        updateDetailedView(flight);
+    }
+    public void updateDetailedView(Flight flight){
+        IDField.setText(flight.getPlaneID());
+        PlaneIDField.setValue(flight.getFlightID());
+        System.out.println(flight.getFlightID());
+        SourceField.setValue(flight.getSource());
+        DestinationField.setValue(flight.getDestination());
+        DateField.setValue(flight.getDate());
+    }
+}
---
+++ Implementation/src/main/java/com/swe2023/gui/AdminPlaneController.java
@@ -0,0 +1,66 @@
+package com.swe2023.gui;
+
+import com.swe2023.HelloApplication;
+import com.swe2023.model.Planes_Data.Plane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+
+public class AdminPlaneController {
+
+    @FXML
+    public Button updateButton;
+    public TextField IDField;
+    public TextField SizeField;
+    public TextField IncomeField;
+    public TextField StatusField;
+    public TextField KindField;
+
+
+    public ListView<String> listView;
+
+    private ArrayList<Plane> Planes;
+    private Plane plane;
+
+    public void initialize(){
+        Planes= new ArrayList<>();
+    }
+    public void goToAdminHome() {
+        HelloApplication.showWindow(updateButton, "/admin-home.fxml", "Administrator", 800,640);
+    }
+
+    public void createPlane(ActionEvent actionEvent) {
+        String id = IDField.getText();
+        String kind = KindField.getText();
+        String status = StatusField.getText();
+        String income = IncomeField.getText();
+        String size = SizeField.getText();
+
+        Plane plane= new Plane(id,status,kind,Integer.parseInt(income),Integer.parseInt(size));
+        Planes.add(plane);
+        updateListView();
+    }
+
+    public void updateListView(){
+        listView.getItems().clear();
+        for(Plane i : Planes)
+            listView.getItems().add(i.getPlaneID());
+        listView.getItems().removeAll();
+    }
+
+
+    public void getViewItem() {
+        int index= listView.getSelectionModel().getSelectedIndex();
+        Plane plane= Planes.get(index);
+        updateDetailedView(plane);
+    }
+    public void updateDetailedView(Plane plane){
+        IDField.setText(plane.getPlaneID());
+        SizeField.setText(String.valueOf(plane.getSize()));
+        KindField.setText(plane.getKind());
+        StatusField.setText(plane.getStatus());
+        IncomeField.setText(String.valueOf(plane.getIncome()));
+    }
+}
