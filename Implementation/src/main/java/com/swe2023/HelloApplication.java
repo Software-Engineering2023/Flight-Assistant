a311425f253c780769c3a0729968bde64e4e44f0
Ziad-H-Mahgoub <Ziad-H-Mahgoub>
2022-01-01 20:35:49 +0200

    gui sgin in

--- Implementation/src/main/java/com/swe2023/HelloApplication.java
+++ Implementation/src/main/java/com/swe2023/HelloApplication.java
@@ -10,16 +10,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        showWindow(null, "/signIn.fxml","Welcome", 800,640);
+
+        showWindow(null, "/signIn.fxml","Welcome", 950,650);
     }
 
     public static void main(String[] args) {
         launch();
     }
 
-    public static void showWindow(Control control, String newWindow ,String title,int v, int v1){
+    public static void showWindow(Control control, String newWindow, String title, int v, int v1){
         try {
             if (control !=null) {
                 Stage st = (Stage) control.getScene().getWindow();
